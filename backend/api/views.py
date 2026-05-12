bcefef4b161641632a34df6256d0e3f0a1841ff2
rodrigojaga <rodrigogalindo3basico@gmail.com>
2026-05-11 22:40:21 -0600

    Configuracion inicial

---
+++ backend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+# Create your views here.
+
+
+def inicio(request):
+    return JsonResponse({
+        "mensaje": "Backend funcionando"
+    })
