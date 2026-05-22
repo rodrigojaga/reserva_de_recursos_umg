22439699af85326bde61e7b2948b4918b0b7c9ac
rodrigojaga <rodrigogalindo3basico@gmail.com>
2026-05-22 16:51:54 -0600

    logica de reservas

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import RecursoListView
+from .views import (
+    RecursoListView,
+    ReservaCreateView,
+    ReservaListView,
+    ReservaCancelarView,
+)
  
 urlpatterns = [
+    # Tarea 2
     path("recursos/", RecursoListView.as_view(), name="recurso-list"),
+ 
+    # Tarea 3
+    path("reservas/",                    ReservaListView.as_view(),    name="reserva-list"),
+    path("reservas/crear/",              ReservaCreateView.as_view(),  name="reserva-crear"),
+    path("reservas/<int:pk>/cancelar/",  ReservaCancelarView.as_view(), name="reserva-cancelar"),
 ]
