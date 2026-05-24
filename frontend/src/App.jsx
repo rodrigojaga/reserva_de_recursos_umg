76b244d207b9658a776131f5acf139f57a17f08d
Notlim <mwmerenxar@gmail.com>
2026-05-23 23:58:55 -0600

    Configuración de Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { NavbarUsuario, NavbarAdmin } from './components/Navbar'; // Importación de las dos barras independientes
 
-// Tus importaciones de páginas
+// Importaciones de tus páginas
 import Login from './pages/Login';
 import Catalogo from './pages/Catalogo';
 import FormularioReserva from './pages/FormularioReserva';
@@ -11,27 +11,33 @@ import PanelAdmin from './pages/PanelAdmin';
 import GestionRecursos from './pages/GestionRecursos';
 import ExportarCSV from './pages/ExportarCSV';
 
-// Creamos un componente intermedio para leer la URL actual de la página
 function ContenidoApp() {
   const location = useLocation();
   
-  // Si la URL empieza con "/admin", sabemos que el usuario es Byron Caal
+  // Evaluamos en qué entorno estamos parados
   const esRutaAdmin = location.pathname.startsWith('/admin');
-  const nombreUsuario = esRutaAdmin ? "Administrador" : "USER";
+  const esLogin = location.pathname === '/';
 
   return (
     <>
-      {/* El Navbar cambia de nombre y título según la página actual */}
-      <Navbar usuario={nombreUsuario} esAdmin={esRutaAdmin} />
+      
+      {!esLogin && (
+        esRutaAdmin 
+          ? <NavbarAdmin usuario="Administrador" /> 
+          : <NavbarUsuario usuario="User" />
+      )}
 
       <Routes>
+        {/* Pantalla de Entrada */}
         <Route path="/" element={<Login />} />
+        
+        {/* Entorno del Usuario Regular */}
         <Route path="/Catalogo" element={<Catalogo />} />
         <Route path="/reservar" element={<FormularioReserva />} />
         <Route path="/mis-reservaciones" element={<MisReservaciones />} />
         <Route path="/consultar" element={<ConsultarCodigo />} />
         
-        {/* Rutas de Administración */}
+        {/* Entorno del Administrador */}
         <Route path="/admin" element={<PanelAdmin />} />
         <Route path="/admin/recursos" element={<GestionRecursos />} />
         <Route path="/admin/reportes" element={<ExportarCSV />} />
