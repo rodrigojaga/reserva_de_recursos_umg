20f50a8478c0ceaf186976439d428cdb5e8b70b1
rodrigojaga <rodrigogalindo3basico@gmail.com>
2026-05-26 21:50:01 -0600

    pruebas concurrencia

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,9 +2,8 @@ from django.db import models
 import random
 import string
 from datetime import date, datetime
-
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.core.exceptions import ValidationError
+#from django.contrib.auth.models import AbstractUser, BaseUserManager
+#from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
